78cdb1441e16c2bb4214e4e0f4db4931f26eac17
Alisa Treyfeld <treyfeld@yandex.ru>
2022-06-10 16:08:45 +0300

    AT10.06 all done

--- sudoku.js
+++ sudoku.js
@@ -5,9 +5,9 @@ const sudokuString = fs.readFileSync('./sudoku-puzzles.txt', 'utf-8');
 function solve(sudokuString) {
   return sudokuString.slice(0, 82).match(/.{1,9}/gm).map((el) => el.split(''));
 }
-console.table(solve(sudokuString));
+
 module.exports = {
   solve,
-  // isSolved,
-  // prettyBoard,
+  isSolved,
+  prettyBoard,
 };
